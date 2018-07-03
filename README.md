54f9c26bcad5ef24ad8bea71d7d075546d967f0b
danielmeek32 <danielmeek32@users.noreply.github.com>
2018-07-03 22:20:13 +0100

    add configurable behaviors when closing a crafting table or the player's inventory
    closes #1

--- README.md
+++ README.md
@@ -15,7 +15,19 @@ Each player will have a 2x2 crafting grid in their inventory in place of the def
     group:wood group:wood
     group:wood group:wood
 
-After placing the crafting table in the world, any player can right-click on it to use a 3x3 crafting grid. Multiple players can use the crafting table at the same time without affecting each other. If a player closes the crafting table form while there are items in the crafting grid, those items will be dropped as entities on top of the crafting table node.
+After placing the crafting table in the world, any player can right-click on it to use a 3x3 crafting grid. Multiple players can use the crafting table at the same time without affecting each other.
+
+By default, if a player closes the crafting table form while there are items in the crafting grid, those items will be dropped as entities on top of the crafting table node. If a player closes their inventory form while there are items in their own crafting grid, those items will be returned to the player's main inventory if possible or dropped on the ground if there is not enough room in the player's inventory. Both of these behaviors are configurable.
+
+## Configuration
+
+This mod provides the following configuration options:
+
+* `craftingtable_crafting_table_close_behavior` - This option controls the behavior used when a player closes a crafting table form while there are items in the crafting grid. The possible values are:
+  - `transfer` - The items from the crafting table will be transferred to the crafting grid shown in the player's inventory form. Any items that do not fit in the player's own crafting grid will be returned to the player's main inventory, or dropped if there is not enough room in the player's inventory.
+  - `return` - The items from the crafting table will be returned to the player's inventory, or dropped if there is not enough room in the player's inventory. The crafting grid shown in the player's inventory form will be empty.
+  - `drop` - The items from the crafting table will be dropped. No items will be added to the player's inventory and the crafting grid shown in the player's inventory form will be empty.
+* `craftingtable_player_inventory_close_behavior` - This option controls the behavior used when a player closes their inventory form while there are items in their own crafting grid. The possible values are the same as for `craftingtable_crafting_table_close_behavior`. Note that the `transfer` behavior effectively does nothing in this case, leaving the items as they are in the player's crafting grid.
 
 ## Dependencies
 
--- init.lua
+++ init.lua
@@ -1,4 +1,171 @@
+local config = {}
+config.crafting_table_close_behavior = minetest.setting_get("craftingtable_crafting_table_close_behavior") or "drop"
+config.player_inventory_close_behavior = minetest.setting_get("craftingtable_player_inventory_close_behavior") or "return"
+
 local open_crafting_tables = {}
+
+-- drop a stack at the specified index on a crafting table at the specified position, unless the crafting table position is nil in which case the item is dropped from the player
+local drop_item_from_player_or_crafting_table = function(stack, player, crafting_table_pos, index)
+	if crafting_table_pos ~= nil then
+		local item_pos = { x = crafting_table_pos.x, y = crafting_table_pos.y + 0.75, z = crafting_table_pos.z }
+
+		-- offset the dropped item position based on its location in the crafting grid and the player's direction
+		local grid_x = (index - 1) % 3
+		local grid_y = math.floor((index - 1) / 3)
+		local grid_x_offset = grid_x * 0.3 - 0.3
+		local grid_y_offset = grid_y * 0.3 - 0.3
+		local player_dir = minetest.dir_to_facedir(player:get_look_dir(), false)
+		if player_dir == 0 then
+			item_pos.x = item_pos.x + grid_x_offset
+			item_pos.z = item_pos.z - grid_y_offset
+		elseif player_dir == 1 then
+			item_pos.x = item_pos.x - grid_y_offset
+			item_pos.z = item_pos.z - grid_x_offset
+		elseif player_dir == 2 then
+			item_pos.x = item_pos.x - grid_x_offset
+			item_pos.z = item_pos.z + grid_y_offset
+		elseif player_dir == 3 then
+			item_pos.x = item_pos.x + grid_y_offset
+			item_pos.z = item_pos.z + grid_x_offset
+		end
+
+		-- add dropped item
+		minetest.add_item(item_pos, stack)
+	else
+		-- drop the item on the ground
+		minetest.item_drop(stack, player, player:get_pos())
+	end
+end
+
+-- transfer as many items from the crafting table to the player's crafting grid as possible, returning those that don't fit to the player's inventory and dropping items if there is not enough space in the player's inventory
+local transfer_items_where_possible = function(player, crafting_table_pos)
+	local inv = player:get_inventory()
+
+	if crafting_table_pos ~= nil then
+		-- find the corners of a square encompassing all the items in the crafting inventory while counting the number of item stacks
+		local stack_count = 0
+		local first_x = 4
+		local first_y = 4
+		local last_x = 0
+		local last_y = 0
+		for index, stack in ipairs(inv:get_list("craft")) do
+			if not stack:is_empty() then
+				local x = ((index - 1) % 3) + 1
+				local y = math.ceil(index / 3)
+
+				if x < first_x then
+					first_x = x
+				end
+				if y < first_y then
+					first_y = y
+				end
+				if x > last_x then
+					last_x = x
+				end
+				if y > last_y then
+					last_y = y
+				end
+
+				stack_count = stack_count + 1
+			end
+		end
+		local width = last_x - first_x + 1
+		local height = last_y - first_y + 1
+
+		if stack_count > 0 then
+			if width <= 2 and height <= 2 then
+				-- gather the items from the relevant area of the crafting inventory into a list, preserving their visual arrangement
+				local stacks = {}
+				for y = first_y, last_y do
+					for x = first_x, last_x do
+						table.insert(stacks, inv:get_stack("craft", (y - 1) * 3 + x))
+					end
+				end
+
+				-- clear the player's crafting inventory
+				for index = 1, 9 do
+					inv:set_stack("craft", index, ItemStack(nil))
+				end
+
+				-- put the items back into the crafting inventory
+				for y = 1, height do
+					for x = 1, width do
+						inv:set_stack("craft", (y - 1) * 2 + x, stacks[(y - 1) * width + x])
+					end
+				end
+			else
+				-- gather all the stacks from the crafting inventory into a list
+				local stacks = {}
+				for index, stack in ipairs(inv:get_list("craft")) do
+					table.insert(stacks, stack)
+
+					-- remove item from crafting inventory (for now)
+					inv:set_stack("craft", index, ItemStack(nil))
+				end
+
+				-- save crafting inventory size and resize it to the smaller size (ugly hack so that we can use inv:add_item to automatically handle adding the items to the crafting inventory)
+				local crafting_inventory_size = inv:get_size("craft")
+				inv:set_size("craft", 4)
+
+				-- try to return as many items as possible to the player's crafting inventory
+				for index, stack in ipairs(stacks) do
+					if not stack:is_empty() then
+						-- try to add the item to the player's crafting inventory
+						local left_over = inv:add_item("craft", stack)
+
+						-- try to add the item to the player's main inventory
+						if left_over ~= nil and not left_over:is_empty() then
+							local left_over = inv:add_item("main", left_over)
+
+							-- drop any left over item
+							if left_over ~= nil and not left_over:is_empty() then
+								drop_item_from_player_or_crafting_table(left_over, player, crafting_table_pos, index)
+							end
+						end
+					end
+				end
+
+				-- restore saved crafting inventory size
+				inv:set_size("craft", crafting_inventory_size)
+			end
+		end
+	end
+end
+
+-- return items from a crafting table or the player's crafting grid to the player's inventory where possible, dropping items if there is not enough space in the player's inventory
+local return_items_where_possible = function(player, crafting_table_pos)
+	local inv = player:get_inventory()
+
+	for index, stack in ipairs(inv:get_list("craft")) do
+		if not stack:is_empty() then
+			-- try to add the item to the player's main inventory
+			local left_over = inv:add_item("main", stack)
+
+			-- drop any left over item
+			if left_over ~= nil and not left_over:is_empty() then
+				drop_item_from_player_or_crafting_table(left_over, player, crafting_table_pos, index)
+			end
+
+			-- remove item from crafting inventory
+			inv:set_stack("craft", index, ItemStack(nil))
+		end
+	end
+end
+
+-- drop all items from a crafting table or the player's crafting grid
+local drop_all_items = function(player, crafting_table_pos)
+	local inv = player:get_inventory()
+
+	for index, stack in ipairs(inv:get_list("craft")) do
+		if not stack:is_empty() then
+			-- drop the item
+			drop_item_from_player_or_crafting_table(stack, player, crafting_table_pos, index)
+
+			-- remove item from crafting inventory
+			inv:set_stack("craft", index, ItemStack(nil))
+		end
+	end
+end
 
 local open_crafting_table = function(player, pos)
 	-- set large crafting inventory size
@@ -31,48 +198,33 @@ local open_crafting_table = function(player, pos)
 end
 
 local handle_crafting_table_close = function(player, pos)
-	local inv = player:get_inventory()
-
-	-- drop all items on the crafting table
-	for index, stack in ipairs(inv:get_list("craft")) do
-		if not stack:is_empty() then
-			local item_pos = { x = pos.x, y = pos.y + 0.75, z = pos.z }
-
-			-- offset the dropped item position based on its location in the crafting grid and the player's direction
-			local grid_x = (index - 1) % 3
-			local grid_y = math.floor((index - 1) / 3)
-			local grid_x_offset = grid_x * 0.3 - 0.3
-			local grid_y_offset = grid_y * 0.3 - 0.3
-			local player_dir = minetest.dir_to_facedir(player:get_look_dir(), false)
-			if player_dir == 0 then
-				item_pos.x = item_pos.x + grid_x_offset
-				item_pos.z = item_pos.z - grid_y_offset
-			elseif player_dir == 1 then
-				item_pos.x = item_pos.x - grid_y_offset
-				item_pos.z = item_pos.z - grid_x_offset
-			elseif player_dir == 2 then
-				item_pos.x = item_pos.x - grid_x_offset
-				item_pos.z = item_pos.z + grid_y_offset
-			elseif player_dir == 3 then
-				item_pos.x = item_pos.x + grid_y_offset
-				item_pos.z = item_pos.z + grid_x_offset
-			end
-
-			-- add dropped item
-			minetest.add_item(item_pos, stack)
-
-			-- remove item from player's crafting inventory
-			stack:clear()
-			inv:set_stack("craft", index, stack)
-		end
+	-- execute configured close behavior
+	if config.crafting_table_close_behavior == "transfer" then
+		transfer_items_where_possible(player, pos)
+	elseif config.crafting_table_close_behavior == "return" then
+		return_items_where_possible(player, pos)
+	elseif config.crafting_table_close_behavior == "drop" then
+		drop_all_items(player, pos)
 	end
 
 	-- restore small crafting inventory size
+	local inv = player:get_inventory()
 	inv:set_width("craft", 2)
 	inv:set_size("craft", 4)
 
 	-- remove player from the list of open crafting tables
 	open_crafting_tables[player:get_player_name()] = nil
+end
+
+local handle_player_inventory_close = function(player)
+	-- execute configured close behavior
+	if config.player_inventory_close_behavior == "transfer" then
+		transfer_items_where_possible(player, nil)
+	elseif config.player_inventory_close_behavior == "return" then
+		return_items_where_possible(player, nil)
+	elseif config.player_inventory_close_behavior == "drop" then
+		drop_all_items(player, nil)
+	end
 end
 
 -- override crafting page with smaller crafting grid
@@ -108,6 +260,8 @@ minetest.register_on_leaveplayer(function(player)
 	local pos = open_crafting_tables[player:get_player_name()]
 	if pos ~= nil then
 		handle_crafting_table_close(player, pos)
+	else
+		handle_player_inventory_close(player)	-- it's safe to always call this function even if the player's inventory wasn't actually open
 	end
 end)
 
@@ -115,14 +269,21 @@ minetest.register_on_dieplayer(function(player)
 	local pos = open_crafting_tables[player:get_player_name()]
 	if pos ~= nil then
 		handle_crafting_table_close(player, pos)
+	else
+		handle_player_inventory_close(player)	-- it's safe to always call this function even if the player's inventory wasn't actually open
 	end
 end)
 
 minetest.register_on_player_receive_fields(function(player, formname, fields)
-	if string.sub(formname, 0, string.len("craftingtable:")) == "craftingtable:" then
-		if fields.quit then
+	if fields.quit then
+		if string.sub(formname, 0, string.len("craftingtable:")) == "craftingtable:" then
+			-- crafting table has been closed
 			local pos = minetest.string_to_pos(string.sub(formname, string.len("craftingtable:") + 1))
 			handle_crafting_table_close(player, pos)
+		elseif formname == "" then
+			-- player inventory has been closed
+			-- TODO: is it safe to assume that an empty formname always refers to the player's inventory? I don't think this matters anyway as it's always safe to call handle_player_inventory_close even when it's not necessary
+			handle_player_inventory_close(player)
 		end
 	end
 end)
